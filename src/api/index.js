5434c7c5d377db617dbbebcb78bcdf5318648f4b
Geovanne Vichiatto <vichiattoge@gmail.com>
2024-11-13 16:05:19 -0300

    Adding a default 'Not Found' response to every non-configured request.

--- src/api/index.js
+++ src/api/index.js
@@ -5,4 +5,8 @@ const router = express.Router();
 
 router.use("/api/awardsInterval/", awardsIntervalRouter);
 
+router.use((req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
 module.exports = router;
